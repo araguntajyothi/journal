99bbfb26ba3e89ca0b9174b113feea7b1a524a39
araguntajyothi <akshayajyothi17@gmail.com>
2025-02-22 13:11:52 +0530

    disabled all test cases

--- src/test/java/com/springboot/rest/api/scheduler/UserSchedulerTests.java
+++ src/test/java/com/springboot/rest/api/scheduler/UserSchedulerTests.java
@@ -1,5 +1,6 @@
 package com.springboot.rest.api.scheduler;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,7 @@ public class UserSchedulerTests {
     @Autowired
     private UserScheduler userScheduler;
 
+    @Disabled
     @Test
     public void testFetchUsersAndSendEmail(){
         userScheduler.fetchUsersAndSendMail();
